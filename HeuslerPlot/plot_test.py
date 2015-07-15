3addcf7f8607425dca476befb000f22c385f20c0
Tim Lovorn <tflovorn@crimson.ua.edu>
2015-07-15 11:02:46 -0500

    distance scaling test

--- HeuslerPlot/plot_test.py
+++ HeuslerPlot/plot_test.py
@@ -1,6 +1,7 @@
 import unittest
-from HeuslerPlot.plot import _cut_duplicates
-from HeuslerPlot.parseVASP import ParseEigenval
+import numpy as np
+from HeuslerPlot.plot import _cut_duplicates, _recip_dist, _scaled_k_xs
+from HeuslerPlot.parseVASP import ParseEigenval, ParseOutcar
 from HeuslerPlot.parseVASP_test import _vec_equal
 
 class TestCutDuplicates(unittest.TestCase):
@@ -16,6 +17,53 @@ class TestCutDuplicates(unittest.TestCase):
 
         # TODO - check that ks and eigenvalues in cut lists match
         # corresponding ones in non-cut lists.
+
+class TestScaledKXs(unittest.TestCase):
+    def test_Co2MnSi(self):
+        ks, eigenvals = ParseEigenval("test_data/e29/Co2MnSi/BANDS/EIGENVAL")
+        E_Fermi, D = ParseOutcar("test_data/e29/Co2MnSi/OUTCAR")
+        R = 2.0 * np.pi * np.linalg.inv(D)
+
+        sym_indices, ks_cut, eigenvals_cut = _cut_duplicates(ks, eigenvals)
+
+        recip_dists = _recip_dist(sym_indices, ks_cut, R)
+        xs = _scaled_k_xs(sym_indices, ks_cut, recip_dists)
+
+        sym_xs = []
+        for i in range(len(sym_indices)):
+            sym_xs.append(xs[sym_indices[i]])
+
+        sym_xs_dists = []
+        for i, x in enumerate(sym_xs):
+            if i != 0:
+                #print(x - sym_xs[i-1])
+                sym_xs_dists.append(x - sym_xs[i-1])
+
+        # Symmetry points from e29/Co2MnSi/BANDS/KPOINTS.
+        # Different from those used by Setyawan (Comp. Mater. Sci. 49, 299 (2010)).
+        # Lattice vectors also differ.
+        G = (0.0, 0.0, 0.0)
+        X = (1/2, 0.0, 0.0)
+        W = (1/2, 1/4, 0.0)
+        K = (3/8, 3/8, 0.0)
+        L = (1/4, 1/4, 1/4)
+        U = (1/2, 1/8, 1/8)
+
+        path = [G, X, W, K, G, L, W, U, X]
+        dists = []
+        for i, k in enumerate(path):
+            k_Cart = np.dot(k, R)
+            #print(k_Cart)
+            if i != 0:
+                prev_k_Cart = np.dot(path[i-1], R)
+                k_to_prev_k = np.subtract(prev_k_Cart, k_Cart)
+                dists.append(np.linalg.norm(k_to_prev_k))
+
+        eps = 1e-9
+        for i, d in enumerate(dists):
+            #print(d / sum(dists))
+            d_norm = d/sum(dists)
+            self.assertTrue(abs(d_norm - sym_xs_dists[i]) < eps)
 
 if __name__ == "__main__":
     unittest.main()
