d11200dd7b9a1c9f5c2916e837b24042774a9f90
Tim Lovorn <tflovorn@crimson.ua.edu>
2015-07-17 11:02:40 -0500

    update plot_test

--- HeuslerPlot/plot_test.py
+++ HeuslerPlot/plot_test.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 from HeuslerPlot.plot import _cut_duplicates, _recip_dist, _scaled_k_xs
 from HeuslerPlot.parseVASP import ParseEigenval, ParseOutcar
-from HeuslerPlot.parseVASP_test import _vec_equal
 
 class TestCutDuplicates(unittest.TestCase):
     def test_Co2MnSi(self):
@@ -22,52 +21,89 @@ class TestCutDuplicates(unittest.TestCase):
 
 class TestScaledKXs(unittest.TestCase):
     def test_Co2MnSi(self):
-        eigenval_path = os.path.join("test_data", "e29", "Co2MnSi", "BANDS", "EIGENVAL")
-        outcar_path = os.path.join("test_data", "e29", "Co2MnSi", "OUTCAR")
+        debug_print = False
+
+        #eigenval_path = os.path.join("test_data", "e29", "Co2MnSi", "BANDS", "EIGENVAL")
+        #outcar_path = os.path.join("test_data", "e29", "Co2MnSi", "OUTCAR")
+        eigenval_path = os.path.join("test_data", "e28", "Co2CrSi", "BANDS", "EIGENVAL")
+        outcar_path = os.path.join("test_data", "e28", "Co2CrSi", "OUTCAR")
         ks, eigenvals = ParseEigenval(eigenval_path)
         E_Fermi, D = ParseOutcar(outcar_path)
         R = 2.0 * np.pi * np.linalg.inv(D)
 
+        if debug_print:
+            for i in range(3):
+                print("D[:, i] = {}".format(str(D[:, i])))
+            for i in range(3):
+                print("R[i, :] = {}".format(str(R[i, :])))
+
         sym_indices, ks_cut, eigenvals_cut = _cut_duplicates(ks, eigenvals)
+
+        if debug_print:
+            print("ks at sym_indices:")
+            for sym_i in sym_indices:
+                print("Reciprocal = {}".format(str(ks_cut[sym_i])))
 
         recip_dists = _recip_dist(sym_indices, ks_cut, R)
         xs = _scaled_k_xs(sym_indices, ks_cut, recip_dists)
 
         sym_xs = []
-        for i in range(len(sym_indices)):
-            sym_xs.append(xs[sym_indices[i]])
+        for sym_i in sym_indices:
+            sym_xs.append(xs[sym_i])
 
+        if debug_print:
+            print("Distance between sym_xs:")
         sym_xs_dists = []
         for i, x in enumerate(sym_xs):
             if i != 0:
-                #print(x - sym_xs[i-1])
+                if debug_print:
+                    print(x - sym_xs[i-1])
                 sym_xs_dists.append(x - sym_xs[i-1])
 
-        # Symmetry points from e29/Co2MnSi/BANDS/KPOINTS.
-        # Different from those used by Setyawan (Comp. Mater. Sci. 49, 299 (2010)).
-        # Lattice vectors also differ.
         G = (0.0, 0.0, 0.0)
-        X = (1/2, 0.0, 0.0)
-        W = (1/2, 1/4, 0.0)
-        K = (3/8, 3/8, 0.0)
-        L = (1/4, 1/4, 1/4)
-        U = (1/2, 1/8, 1/8)
+        X = (1/2, 0.0, 1/2)
+        W = (1/2, 1/4, 3/4)
+        K = (3/8, 3/8, 3/4)
+        L = (1/2, 1/2, 1/2)
+        U = (5/8, 1/4, 5/8)
 
+        if debug_print:
+            print("Expected ks as sym_indices:")
         path = [G, X, W, K, G, L, W, U, X]
-        dists = []
+        expected_dists = []
         for i, k in enumerate(path):
             k_Cart = np.dot(k, R)
-            #print(k_Cart)
+            if debug_print:
+                print("Reciprocal = {}; Cartesian = {}".format(str(k), str(k_Cart)))
             if i != 0:
                 prev_k_Cart = np.dot(path[i-1], R)
                 k_to_prev_k = np.subtract(prev_k_Cart, k_Cart)
-                dists.append(np.linalg.norm(k_to_prev_k))
+                expected_dists.append(np.linalg.norm(k_to_prev_k))
 
+        if debug_print:
+            print("Expected distance between sym_xs:")
         eps = 1e-9
-        for i, d in enumerate(dists):
-            #print(d / sum(dists))
-            d_norm = d/sum(dists)
+        expected_dists_norm = []
+        for i, d in enumerate(expected_dists):
+            d_norm = d/sum(expected_dists)
+            if debug_print:
+                print(d_norm)
+            expected_dists_norm.append(d_norm)
             self.assertTrue(abs(d_norm - sym_xs_dists[i]) < eps)
+
+        sym_xs_vals = [0.0]
+        expected_xs_vals = [0.0]
+        for i, dist in enumerate(sym_xs_dists):
+            sym_xs_vals.append(sym_xs_vals[i] + dist)
+        for i, expected_dist in enumerate(expected_dists_norm):
+            expected_xs_vals.append(expected_xs_vals[i] + expected_dist)
+        if debug_print:
+            print("Normalized xs values:")
+            print(sym_xs_vals)
+            print("Expected normalized xs values:")
+            print(expected_xs_vals)
+        for i, expected_val in enumerate(expected_xs_vals):
+            self.assertTrue(abs(expected_val - sym_xs_vals[i]) < eps)
 
 if __name__ == "__main__":
     unittest.main()
